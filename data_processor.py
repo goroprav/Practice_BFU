aea764ad293389a5c3b76f0297d472558847b434
goroprav <56308902+goroprav@users.noreply.github.com>
2024-05-23 13:08:25 +0200

    Add files via upload

--- data_processor.py
+++ data_processor.py
@@ -1,17 +1,33 @@
+import math
+
 class DataProcessor:
-    def __init__(self, data, coefficient_1, coefficient_2):
-        self.data = data
+    def __init__(self, data_x, data_y, coefficient_1, coefficient_2, lambda_0, temp_T):
+        self.data_x = data_x
+        self.data_y = data_y
         self.coefficient_1 = coefficient_1
         self.coefficient_2 = coefficient_2
+        self.lambda_0 = lambda_0
+        self.temp_T = temp_T
 
     def process_data(self):
-        processed_data = []
-        for value in self.data:
+        processed_data_x = []
+        processed_data_y = []
+        c=299792458
+        pi=3.14159265358979
+        h=6.626069573*10**(-34)
+        k=1.380649*10**(-23)
+        v_0=c/self.lambda_0
+
+
+        for x, y in zip(self.data_x, self.data_y):
             try:
-                value = float(value)  # Преобразование строки в число
-                new_value = value * self.coefficient_1 + self.coefficient_2
-                processed_data.append(new_value)
+                x = float(x)
+                y = float(y)
+                new_x = x * (x * self.coefficient_1 + self.coefficient_2)
+                v_j=(new_x*c)/(2*pi)
+                new_y = ( (2**4) * (pi**4) )/45 * ((v_0 - v_j)**4/(1-math.exp((-h*c*v_j)/(k*self.temp_T)))) * h/(8* (pi**4) * c * v_j) * y
+                processed_data_x.append(new_x)
+                processed_data_y.append(new_y)
             except ValueError:
-                # Если значение не может быть преобразовано в float, пропускаем его
                 continue
-        return processed_data
+        return processed_data_x, processed_data_y
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from tkinter import ttk
 import pandas as pd
 from data_loader import DataLoader
 from data_processor import DataProcessor
@@ -7,38 +8,61 @@ from data_processor import DataProcessor
 class DataApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Приложение для обработки данных")
+        self.root.title("Обработчик")
         self.root.geometry("800x600")
+        self.root.configure(bg='#2f2f2f')  # Темно-серый фон
+
+        style = ttk.Style()
+        style.configure('TButton', 
+                        background='white', 
+                        foreground='black', 
+                        borderwidth=1, 
+                        relief="solid")
+        style.configure('TEntry', 
+                        fieldbackground='black', 
+                        foreground='white', 
+                        borderwidth=1, 
+                        relief="solid")
+        style.configure('TLabel', 
+                        background='#2f2f2f', 
+                        foreground='white')
+        style.configure('TOptionMenu', 
+                        background='black', 
+                        foreground='black', 
+                        borderwidth=1, 
+                        relief="solid")
 
         self.data = None
-        self.processed_data = None
+        self.processed_data_x = None
+        self.processed_data_y = None
 
-        self.upload_button = tk.Button(root, text="Загрузить файл", command=self.upload_file)
-        self.upload_button.pack()
+        self.upload_button = ttk.Button(root, text="Загрузить файл", command=self.upload_file)
+        self.upload_button.grid(row=0, column=0, columnspan=4, pady=10, padx=10)
 
         self.data_selection = tk.StringVar(root)
-        self.data_selection.set("X1")  # Значение по умолчанию
-        self.data_menu = tk.OptionMenu(root, self.data_selection, "X1", "Y1", "X2", "Y2", command=self.display_selected_data)
-        self.data_menu.pack()
+        self.data_selection.set("X1 и Y1")  # Значение по умолчанию
+        self.data_menu = ttk.OptionMenu(root, self.data_selection, "X1 и Y1", "X1 и Y1", "X2 и Y2", command=self.display_selected_data)
+        self.data_menu.grid(row=1, column=0, columnspan=4, pady=10, padx=10)
 
-        self.coefficient_1_label = tk.Label(root, text="Коэффициент 1:")
-        self.coefficient_1_label.pack()
-        self.coefficient_1_entry = tk.Entry(root)
-        self.coefficient_1_entry.pack()
+        self.coefficient_1_label = ttk.Label(root, text="Коэффициент 1:")
+        self.coefficient_1_entry = ttk.Entry(root)
 
-        self.coefficient_2_label = tk.Label(root, text="Коэффициент 2:")
-        self.coefficient_2_label.pack()
-        self.coefficient_2_entry = tk.Entry(root)
-        self.coefficient_2_entry.pack()
+        self.coefficient_2_label = ttk.Label(root, text="Коэффициент 2:")
+        self.coefficient_2_entry = ttk.Entry(root)
 
-        self.process_button = tk.Button(root, text="Обработать данные", command=self.process_data)
-        self.process_button.pack()
+        self.lambda_0_label = ttk.Label(root, text="lambda_0:")
+        self.lambda_0_entry = ttk.Entry(root)
 
-        self.output_text = tk.Text(root, height=20, width=80)
-        self.output_text.pack()
+        self.temp_T_label = ttk.Label(root, text="temp_T:")
+        self.temp_T_entry = ttk.Entry(root)
 
-        self.save_button = tk.Button(root, text="Сохранить в Excel", command=self.save_to_excel)
-        self.save_button.pack()
+        self.process_button = ttk.Button(root, text="Обработать данные", command=self.process_data)
+
+        self.output_text = tk.Text(root, height=20, width=100, bg='black', fg='white', wrap='none')
+
+        self.save_button = ttk.Button(root, text="Сохранить в Excel", command=self.save_to_excel)
+
+        self.show_or_hide_widgets()
 
     def upload_file(self):
         file_path = filedialog.askopenfilename()
@@ -54,17 +78,43 @@ class DataApp:
 
         self.output_text.delete('1.0', tk.END)
         selected_data = self.data_selection.get()
-        if selected_data == "X1":
-            data_to_display = self.data[0]
-        elif selected_data == "Y1":
-            data_to_display = self.data[1]
-        elif selected_data == "X2":
-            data_to_display = self.data[2]
-        elif selected_data == "Y2":
-            data_to_display = self.data[3]
+        if selected_data == "X1 и Y1":
+            data_x = self.data[0]
+            data_y = self.data[1]
+        elif selected_data == "X2 и Y2":
+            data_x = self.data[2]
+            data_y = self.data[3]
 
-        for value in data_to_display:
-            self.output_text.insert(tk.END, f"{value}\n")
+        for x, y in zip(data_x, data_y):
+            self.output_text.insert(tk.END, f"{x}\t\t{y}\n")
+
+        self.show_or_hide_widgets()
+
+    def show_or_hide_widgets(self):
+        if self.data_selection.get() == "X1 и Y1":
+            self.coefficient_1_label.grid(row=2, column=0, pady=10, padx=10, sticky='e')
+            self.coefficient_1_entry.grid(row=2, column=1, pady=10, padx=10, sticky='w')
+            self.coefficient_2_label.grid(row=3, column=0, pady=10, padx=10, sticky='e')
+            self.coefficient_2_entry.grid(row=3, column=1, pady=10, padx=10, sticky='w')
+            self.lambda_0_label.grid_remove()
+            self.lambda_0_entry.grid_remove()
+            self.temp_T_label.grid_remove()
+            self.temp_T_entry.grid_remove()
+            self.process_button.grid(row=4, column=0, columnspan=4, pady=10, padx=10)
+            self.output_text.grid(row=5, column=0, columnspan=4, pady=10, padx=10)
+            self.save_button.grid(row=6, column=0, columnspan=4, pady=10, padx=10)
+        else:
+            self.coefficient_1_label.grid(row=2, column=0, pady=10, padx=10, sticky='e')
+            self.coefficient_1_entry.grid(row=2, column=1, pady=10, padx=10, sticky='w')
+            self.coefficient_2_label.grid(row=3, column=0, pady=10, padx=10, sticky='e')
+            self.coefficient_2_entry.grid(row=3, column=1, pady=10, padx=10, sticky='w')
+            self.lambda_0_label.grid(row=2, column=2, pady=10, padx=10, sticky='e')
+            self.lambda_0_entry.grid(row=2, column=3, pady=10, padx=10, sticky='w')
+            self.temp_T_label.grid(row=3, column=2, pady=10, padx=10, sticky='e')
+            self.temp_T_entry.grid(row=3, column=3, pady=10, padx=10, sticky='w')
+            self.process_button.grid(row=4, column=0, columnspan=4, pady=10, padx=10)
+            self.output_text.grid(row=5, column=0, columnspan=4, pady=10, padx=10)
+            self.save_button.grid(row=6, column=0, columnspan=4, pady=10, padx=10)
 
     def process_data(self):
         if not self.data:
@@ -72,47 +122,47 @@ class DataApp:
             return
 
         selected_data = self.data_selection.get()
-        if selected_data == "X1":
-            data_to_process = self.data[0]
-        elif selected_data == "Y1":
-            data_to_process = self.data[1]
-        elif selected_data == "X2":
-            data_to_process = self.data[2]
-        elif selected_data == "Y2":
-            data_to_process = self.data[3]
+        if selected_data == "X1 и Y1":
+            data_x = self.data[0]
+            data_y = self.data[1]
+        elif selected_data == "X2 и Y2":
+            data_x = self.data[2]
+            data_y = self.data[3]
 
         try:
             coefficient_1 = float(self.coefficient_1_entry.get())
             coefficient_2 = float(self.coefficient_2_entry.get())
+            lambda_0 = float(self.lambda_0_entry.get())*10**(-9)
+            temp_T = float(self.temp_T_entry.get())
         except ValueError:
             messagebox.showerror("Ошибка", "Введите корректные значения для коэффициентов")
             return
 
-        processor = DataProcessor(data_to_process, coefficient_1, coefficient_2)
-        self.processed_data = processor.process_data()
+        processor = DataProcessor(data_x, data_y, coefficient_1, coefficient_2, lambda_0, temp_T)
+        self.processed_data_x, self.processed_data_y = processor.process_data()
 
         self.output_text.delete('1.0', tk.END)
-        for original, new in zip(data_to_process, self.processed_data):
-            self.output_text.insert(tk.END, f"{original}\t\t{new}\n")
+        for x, y, new_x, new_y in zip(data_x, data_y, self.processed_data_x, self.processed_data_y):
+            self.output_text.insert(tk.END, f"{x}\t\t{y}\t\t{new_x}\t\t{new_y}\n")
 
     def save_to_excel(self):
-        if not self.processed_data:
+        if not self.processed_data_x or not self.processed_data_y:
             messagebox.showerror("Ошибка", "Сначала обработайте данные")
             return
 
         selected_data = self.data_selection.get()
-        if selected_data == "X1":
-            original_data = self.data[0]
-        elif selected_data == "Y1":
-            original_data = self.data[1]
-        elif selected_data == "X2":
-            original_data = self.data[2]
-        elif selected_data == "Y2":
-            original_data = self.data[3]
+        if selected_data == "X1 и Y1":
+            original_data_x = self.data[0]
+            original_data_y = self.data[1]
+        elif selected_data == "X2 и Y2":
+            original_data_x = self.data[2]
+            original_data_y = self.data[3]
 
         df = pd.DataFrame({
-            'Original Data': original_data,
-            'Processed Data': self.processed_data
+            'Original X': original_data_x,
+            'Original Y': original_data_y,
+            'Processed X': self.processed_data_x,
+            'Processed Y': self.processed_data_y
         })
 
         file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
