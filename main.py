2f5e700226466fdc09e6690e9e74f0d147a14762
goroprav <56308902+goroprav@users.noreply.github.com>
2024-06-03 12:22:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QTextEdit, QFileDialog, 
                              QMessageBox, QVBoxLayout, QHBoxLayout, QWidget, QSpacerItem, QSizePolicy)
+
 import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+
 from data_loader import DataLoader
 from data_processor import DataProcessor
 
@@ -9,9 +13,9 @@ class DataApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Обработчик")
-        self.setGeometry(100, 100, 800, 600)
+        self.setGeometry(100, 100, 900, 600)
         
-        self.setStyleSheet("background-color: lightblue;")  # Задний фон светло-голубого цвета
+        self.setStyleSheet("background-color: #EAEAFC;")  # Задний фон светло-голубого цвета
 
         self.data = None
         self.processed_data_x = None
@@ -24,36 +28,43 @@ class DataApp(QMainWindow):
         self.upload_button = QPushButton("Загрузить файл", self)
         self.upload_button.clicked.connect(self.upload_file)
         self.upload_button.setStyleSheet(self.get_button_style())
+        
         input_layout.addWidget(self.upload_button)
 
-        self.button_x1_y1 = QPushButton("X1 и Y1", self)
-        self.button_x1_y1.clicked.connect(lambda: self.select_data_set("X1 и Y1"))
+        self.button_x1_y1 = QPushButton("Пересчет Peak information", self)
         self.button_x1_y1.setCheckable(True)
-        self.button_x1_y1.setStyleSheet(self.get_toggle_button_style())
+        self.button_x1_y1.clicked.connect(self.select_peak_information)
+        self.button_x1_y1.setStyleSheet(self.get_toggle_button_style(False))
+        
         input_layout.addWidget(self.button_x1_y1)
 
-        self.button_x2_y2 = QPushButton("X2 и Y2", self)
-        self.button_x2_y2.clicked.connect(lambda: self.select_data_set("X2 и Y2"))
+        self.button_x2_y2 = QPushButton("Пересчет Plot Curves", self)
         self.button_x2_y2.setCheckable(True)
-        self.button_x2_y2.setStyleSheet(self.get_toggle_button_style())
+        self.button_x2_y2.clicked.connect(self.select_plot_curves)
+        self.button_x2_y2.setStyleSheet(self.get_toggle_button_style(False))
+        
         input_layout.addWidget(self.button_x2_y2)
 
-        self.coefficient_1_label = QLabel("Коэффициент 1:", self)
+        self.coefficient_1_label = QLabel("Коэффициент a:", self)
+        self.coefficient_1_label.setStyleSheet("color: black;")
         input_layout.addWidget(self.coefficient_1_label)
         self.coefficient_1_entry = QLineEdit(self)
         self.coefficient_1_entry.setStyleSheet(self.get_line_edit_style())
         input_layout.addWidget(self.coefficient_1_entry)
 
-        self.coefficient_2_label = QLabel("Коэффициент 2:", self)
+        self.coefficient_2_label = QLabel("Коэффициент b:", self)
+        self.coefficient_2_label.setStyleSheet("color: black;")
         input_layout.addWidget(self.coefficient_2_label)
         self.coefficient_2_entry = QLineEdit(self)
         self.coefficient_2_entry.setStyleSheet(self.get_line_edit_style())
         input_layout.addWidget(self.coefficient_2_entry)
 
-        self.lambda_0_label = QLabel("lambda_0:", self)
+        self.lambda_0_label = QLabel("Длина волны:", self)
+        self.lambda_0_label.setStyleSheet("color: black;")
         self.lambda_0_entry = QLineEdit(self)
         self.lambda_0_entry.setStyleSheet(self.get_line_edit_style())
-        self.temp_T_label = QLabel("temp_T:", self)
+        self.temp_T_label = QLabel("Температура:", self)
+        self.temp_T_label.setStyleSheet("color: black;")
         self.temp_T_entry = QLineEdit(self)
         self.temp_T_entry.setStyleSheet(self.get_line_edit_style())
 
@@ -67,13 +78,13 @@ class DataApp(QMainWindow):
         self.temp_T_label.hide()
         self.temp_T_entry.hide()
 
-        # Add a spacer to push the elements to the top
-        spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        spacer = QSpacerItem(12, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
         input_layout.addItem(spacer)
 
         self.process_button = QPushButton("Обработать данные", self)
         self.process_button.clicked.connect(self.process_data)
         self.process_button.setStyleSheet(self.get_button_style())
+       
         output_layout.addWidget(self.process_button)
 
         self.output_text = QTextEdit(self)
@@ -83,10 +94,11 @@ class DataApp(QMainWindow):
         self.save_button = QPushButton("Сохранить в Excel", self)
         self.save_button.clicked.connect(self.save_to_excel)
         self.save_button.setStyleSheet(self.get_button_style())
+      
         output_layout.addWidget(self.save_button)
 
-        main_layout.addLayout(input_layout, 1)  # Левая часть занимает 25% ширины
-        main_layout.addLayout(output_layout, 3)  # Правая часть занимает 75% ширины
+        main_layout.addLayout(input_layout, 1)
+        main_layout.addLayout(output_layout, 3)
 
         container = QWidget()
         container.setLayout(main_layout)
@@ -98,19 +110,33 @@ class DataApp(QMainWindow):
             loader = DataLoader(file_path)
             self.data = loader.load_data()
             QMessageBox.information(self, "Загрузка файла", "Файл успешно загружен")
+            self.temp_T_label.setStyleSheet("color: black;")
 
-    def select_data_set(self, selected):
+    def select_peak_information(self):
         if not self.data:
             QMessageBox.critical(self, "Ошибка", "Сначала загрузите файл")
+            self.button_x1_y1.setChecked(False)
             return
+        self.button_x1_y1.setChecked(True)
+        self.button_x1_y1.setStyleSheet(self.get_toggle_button_style(False))
+        self.button_x2_y2.setChecked(False)
+        self.button_x2_y2.setStyleSheet(self.get_toggle_button_style(False))
+        self.select_data_set("Пересчет Peak information")
 
-        self.button_x1_y1.setChecked(selected == "X1 и Y1")
-        self.button_x2_y2.setChecked(selected == "X2 и Y2")
+    def select_plot_curves(self):
+        if not self.data:
+            QMessageBox.critical(self, "Ошибка", "Сначала загрузите файл")
+            self.button_x2_y2.setChecked(False)
+            return
+        self.button_x2_y2.setChecked(True)
+        self.button_x2_y2.setStyleSheet(self.get_toggle_button_style(False))
+        self.button_x1_y1.setChecked(False)
+        self.button_x1_y1.setStyleSheet(self.get_toggle_button_style(False))
+        self.select_data_set("Пересчет Plot Curves")
 
+    def select_data_set(self, selected):
         self.output_text.clear()
-        if selected == "X1 и Y1":
-            self.button_x1_y1.setStyleSheet(self.get_active_toggle_button_style())
-            self.button_x2_y2.setStyleSheet(self.get_toggle_button_style())
+        if selected == "Пересчет Peak information":
             data_x = self.data[0]
             for x in data_x:
                 self.output_text.append(f"{x}")
@@ -120,12 +146,10 @@ class DataApp(QMainWindow):
             self.temp_T_label.hide()
             self.temp_T_entry.hide()
         else:
-            self.button_x2_y2.setStyleSheet(self.get_active_toggle_button_style())
-            self.button_x1_y1.setStyleSheet(self.get_toggle_button_style())
             data_x = self.data[2]
             data_y = self.data[3]
             for x, y in zip(data_x, data_y):
-                self.output_text.append(f"{x}\t\t{y}")
+                self.output_text.append(f"{x}\t{y}")
 
             self.lambda_0_label.show()
             self.lambda_0_entry.show()
@@ -137,13 +161,11 @@ class DataApp(QMainWindow):
             QMessageBox.critical(self, "Ошибка", "Сначала загрузите файл")
             return
 
-        self.output_text.clear()  # Clear old data before processing new data
-
-        selected_data = "X1 и Y1" if self.button_x1_y1.isChecked() else "X2 и Y2"
+        selected_data = "Пересчет Peak information" if self.button_x1_y1.isChecked() else "Пересчет Plot Curves"
         try:
             coefficient_1 = float(self.coefficient_1_entry.text())
             coefficient_2 = float(self.coefficient_2_entry.text())
-            if selected_data == "X1 и Y1":
+            if selected_data == "Пересчет Peak information":
                 data_x = self.data[0]
                 processor = DataProcessor(data_x, coefficient_1=coefficient_1, coefficient_2=coefficient_2)
                 self.processed_data_x = processor.process_x1()
@@ -160,96 +182,129 @@ class DataApp(QMainWindow):
             return
 
         self.output_text.clear()
-        if selected_data == "X1 и Y1":
-            for x, new_x in zip(data_x, self.processed_data_x):
+        if selected_data == "Пересчет Peak information":
+            for x, new_x in zip(self.data[0], self.processed_data_x):
                 self.output_text.append(f"{x}\t\t{new_x}")
         else:
-            for x, y, new_x, new_y in zip(data_x, data_y, self.processed_data_x, self.processed_data_y):
-                self.output_text.append(f"{x}\t\t{y}\t\t{new_x}\t\t{new_y}")
+            for x, y, new_x, new_y in zip(self.data[2], self.data[3], self.processed_data_x, self.processed_data_y):
+                self.output_text.append(f"{x}\t{y}\t{new_x}\t{new_y}")
 
     def save_to_excel(self):
-        if not self.processed_data_x:
-            QMessageBox.critical(self, "Ошибка", "Сначала обработайте данные")
+        if self.processed_data_x is None:
+            QMessageBox.critical(self, "Ошибка", "Нет данных для сохранения")
             return
 
-        selected_data = "X1 и Y1" if self.button_x1_y1.isChecked() else "X2 и Y2"
-        if selected_data == "X1 и Y1":
-            original_data_x = self.data[0]
-            df = pd.DataFrame({
-                'Original X': original_data_x,
-                'Processed X': self.processed_data_x
-            })
-        else:
-            original_data_x = self.data[2]
-            original_data_y = self.data[3]
-            df = pd.DataFrame({
-                'Original X': original_data_x,
-                'Original Y': original_data_y,
-                'Processed X': self.processed_data_x,
-                'Processed Y': self.processed_data_y
-            })
-
-        file_path, _ = QFileDialog.getSaveFileName(self, "Сохранить в Excel", "", "Excel files (*.xlsx)")
+        file_path, _ = QFileDialog.getSaveFileName(self, "Сохранить файл", "", "Excel Files (*.xlsx)")
         if file_path:
-            df.to_excel(file_path, index=False)
+            if self.button_x1_y1.isChecked():
+                df = pd.DataFrame({
+                    "X": self.data[0],
+                    "Корректированная X": self.processed_data_x
+                })
+                coefficients = pd.DataFrame({
+                    "Coefficient a": [float(self.coefficient_1_entry.text())],
+                    "Coefficient b": [float(self.coefficient_2_entry.text())]
+                })
+            else:
+                df = pd.DataFrame({
+                    "X": self.data[2],
+                    "Y": self.data[3],
+                    "Корректированная X": self.processed_data_x,
+                    "Корректированная Y": self.processed_data_y
+                })
+                coefficients = pd.DataFrame({
+                    "Coefficient 1": [float(self.coefficient_1_entry.text())],
+                    "Coefficient 2": [float(self.coefficient_2_entry.text())],
+                    "Lambda 0": [float(self.lambda_0_entry.text())],
+                    "Temp T": [float(self.temp_T_entry.text())]
+                })
+
+            with pd.ExcelWriter(file_path) as writer:
+                df.to_excel(writer, sheet_name='Processed Data', index=False)
+                coefficients.to_excel(writer, sheet_name='Coefficients', index=False)
+
+            self.adjust_column_widths(file_path, 'Processed Data')
+            self.adjust_column_widths(file_path, 'Coefficients')
+
             QMessageBox.information(self, "Сохранение файла", "Файл успешно сохранен")
+
+    def adjust_column_widths(self, file_path, sheet_name):
+        workbook = load_workbook(file_path)
+        sheet = workbook[sheet_name]
+        for column in sheet.columns:
+            max_length = 0
+            column_letter = get_column_letter(column[0].column)
+            for cell in column:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(cell.value)
+                except:
+                    pass
+            adjusted_width = (max_length + 2) * 2
+            sheet.column_dimensions[column_letter].width = adjusted_width
+        workbook.save(file_path)
+
 
     def get_button_style(self):
         return """
         QPushButton {
-            background-color: blue;
+            background-color: #21145F;
             color: white;
             border-radius: 10px;
-            padding: 15px;
-            font-size: 16px;
+            padding: 10px;
+            min-width: 160px;
         }
         QPushButton:hover {
-            background-color: darkblue;
+            background-color: #1A1047;
         }
         """
 
-    def get_toggle_button_style(self):
-        return """
-        QPushButton {
-            background-color: lightgray;
-            color: black;
-            border-radius: 10px;
-            padding: 15px;
-            font-size: 16px;
-        }
-        QPushButton:checked {
-            background-color: blue;
-            color: white;
-            border-radius: 10px;
-        }
-        """
-
-    def get_active_toggle_button_style(self):
-        return """
-        QPushButton {
-            background-color: blue;
-            color: blue;
-            border-radius: 10px;
-        }
-        QPushButton:hover {
-            background-color: darkblue;
-            border-radius: 10px;
-        }
-        """
+    def get_toggle_button_style(self, active):
+        if active:
+            return """
+            QPushButton {
+                background-color: #21145F;
+                color: white;
+                border-radius: 10px;
+                min-width: 160px;
+            }
+            QPushButton:hover {
+                background-color: #21145F;
+                color: white;
+                border-radius: 10px;
+                min-width: 160px;
+            }
+            """
+        else:
+            return """
+            QPushButton {
+                background-color: white;
+                color: black;
+                border-radius: 10px;
+                padding: 10px;
+                min-width: 160px;
+            }
+            QPushButton:hover {
+                background-color: #21145F;
+                color: white;
+                border-radius: 10px;
+                padding: 10px;
+            }
+            """
 
     def get_line_edit_style(self):
         return """
         QLineEdit {
             background-color: white;
-            border: 2px solid blue;
+            border-radius: 10px;
+            color: black;
+            padding: 6px;
+        }
+        QLineEdit:focus {
+            background-color: white;
             border-radius: 10px;
             color: black;
             padding: 5px;
-            outline: none;  /* Убираем обводку */
-        }
-        QLineEdit:focus {
-            border: 2px solid darkblue;
-            outline: none;  /* Убираем обводку */
         }
         """
 
@@ -257,30 +312,37 @@ class DataApp(QMainWindow):
         return """
         QTextEdit {
             background-color: white;
-            border: 2px solid blue;
             border-radius: 10px;
             color: black;
             padding: 10px;
-            font-size: 12px;  /* Уменьшенный размер текста */
+            font-size: 12px;
+            tab-width: 2;
         }
         QTextEdit QScrollBar:vertical {
-            background-color: blue;
+            background-color: white;
             width: 14px;
+            color: white;
             border-radius: 7px;
         }
         QTextEdit QScrollBar::handle:vertical {
-            background-color: lightblue;
+            background-color: #21145F;
             min-height: 20px;
             border-radius: 7px;
         }
         QTextEdit QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
-            background: none;
-            border-radius: 7px;
+            background-color: white;
+        }
+        QTextEdit QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {
+            background-color: white;
+        }
+        QTextEdit QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {
+            background-color: white;
         }
         """
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
     window = DataApp()
     window.show()
     sys.exit(app.exec_())
